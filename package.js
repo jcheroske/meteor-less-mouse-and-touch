fe3e082612e043336cdc4629f234a76c39a05c25
Jay Cheroske <jay@portal.org>
2016-03-03 21:22:28 -0700

    Initial commit

---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  name: 'jcheroske:less-mouse-and-touch',
+  version: '0.0.1',
+  summary: 'LESS mixins that enable easy styling of mouse and touchpad events.',
+  git: 'https://github.com/jcheroske/meteor-less-mouse-and-touch.git',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.addFiles('imports/mouse-and-touch.import.less', ['client']);
+});
